56994c306659470a03bd0d7fad6d124da5c6189e
VkTheEncoder <158612790+VkTheEncoder@users.noreply.github.com>
2025-08-14 14:12:20 +0530

    Update dbhelper.py

--- helper_func/dbhelper.py
+++ helper_func/dbhelper.py
@@ -3,23 +3,19 @@ import sqlite3
 class Database:
 
     def __init__(self):
-
-        self.conn = sqlite3.connect('muxdb.sqlite', check_same_thread = False)
+        self.conn = sqlite3.connect('muxdb.sqlite', check_same_thread=False)
 
     def setup(self):
-
         cmd = """CREATE TABLE IF NOT EXISTS muxbot(
         user_id INT,
         vid_name TEXT,
         sub_name TEXT,
         filename TEXT
         );"""
-
         self.conn.execute(cmd)
         self.conn.commit()
 
     def put_video(self, user_id, vid_name, filename):
-
         ins_cmd = 'INSERT INTO muxbot VALUES (?,?,?,?);'
         srch_cmd = f'SELECT * FROM muxbot WHERE user_id={user_id};'
         up_cmd = f'UPDATE muxbot SET vid_name="{vid_name}", filename="{filename}" WHERE user_id={user_id};'
@@ -28,88 +24,100 @@ class Database:
         if res:
             self.conn.execute(up_cmd)
             self.conn.commit()
-        else :
-            self.conn.execute(ins_cmd,data)
+        else:
+            self.conn.execute(ins_cmd, data)
             self.conn.commit()
 
-    def put_sub(self,user_id,sub_name) :
-
+    def put_sub(self, user_id, sub_name):
         ins_cmd = 'INSERT INTO muxbot VALUES (?,?,?,?);'
         srch_cmd = f'SELECT * FROM muxbot WHERE user_id={user_id};'
         up_cmd = f'UPDATE muxbot SET sub_name="{sub_name}" WHERE user_id={user_id};'
-        data = (user_id,None,sub_name,None)
+        data = (user_id, None, sub_name, None)
 
         res = self.conn.execute(srch_cmd).fetchone()
-        if res :
+        if res:
             self.conn.execute(up_cmd)
             self.conn.commit()
-        else :
-            self.conn.execute(ins_cmd,data)
+        else:
+            self.conn.execute(ins_cmd, data)
             self.conn.commit()
 
-    def check_sub(self,user_id) :
-
+    def check_sub(self, user_id):
         srch_cmd = f'SELECT * FROM muxbot WHERE user_id={user_id};'
-
         res = self.conn.execute(srch_cmd).fetchone()
-        if res :
-
+        if res:
             sub_file = res[2]
-            if sub_file :
+            if sub_file:
                 return True
-            else :
+            else:
                 return False
-
-        else :
+        else:
             return False
 
-    def check_video(self,user_id) :
-
+    def check_video(self, user_id):
         srch_cmd = f'SELECT * FROM muxbot WHERE user_id={user_id};'
         res = self.conn.execute(srch_cmd).fetchone()
-        if res :
+        if res:
             vid_file = res[1]
-            if vid_file :
+            if vid_file:
                 return True
-            else :
+            else:
                 return False
-        else :
+        else:
             return False
 
-    def get_vid_filename(self, user_id) :
-
+    def get_vid_filename(self, user_id):
         cmd = f'SELECT * FROM muxbot WHERE user_id={user_id};'
         res = self.conn.execute(cmd).fetchone()
-        if res :
+        if res:
             return res[1]
-        else :
+        else:
             return False
 
-    def get_sub_filename(self, user_id) :
-
+    def get_sub_filename(self, user_id):
         cmd = f'SELECT * FROM muxbot WHERE user_id={user_id};'
         res = self.conn.execute(cmd).fetchone()
-        if res :
+        if res:
             return res[2]
-        else :
+        else:
             return False
 
-    def get_filename(self, user_id) :
-
+    def get_filename(self, user_id):
         cmd = f'SELECT * FROM muxbot WHERE user_id={user_id};'
         res = self.conn.execute(cmd).fetchone()
-        if res :
+        if res:
             return res[3]
-        else :
+        else:
             return False
 
-    def erase(self,user_id) :
-
+    def erase(self, user_id):
         erase_cmd = f'DELETE FROM muxbot WHERE user_id={user_id} ;'
-
-        try :
+        try:
             self.conn.execute(erase_cmd)
             self.conn.commit()
             return True
-        except :
+        except:
             return False
+
+    # =========================
+    # Compatibility shims (NEW)
+    # =========================
+
+    def get_video(self, user_id: int) -> str:
+        """Compat wrapper for newer code paths."""
+        return self.get_vid_filename(user_id)
+
+    def get_sub(self, user_id: int) -> str:
+        """Compat wrapper for newer code paths."""
+        return self.get_sub_filename(user_id)
+
+    def set_video(self, user_id: int, vid_name: str, filename: str | None = None):
+        """
+        Compat wrapper: newer code may call set_video(user_id, vid_name, original_name?).
+        Falls back to vid_name if a separate original filename isn't provided.
+        """
+        return self.put_video(user_id, vid_name, filename or vid_name)
+
+    def set_sub(self, user_id: int, sub_name: str):
+        """Compat wrapper for newer code paths."""
+        return self.put_sub(user_id, sub_name)
